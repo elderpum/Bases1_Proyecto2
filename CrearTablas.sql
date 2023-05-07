49af4af39770bd86fc77f5c28cd45b79e4c8043f
Elder Pum <ElderPum@gmail.com>
2023-05-06 23:05:12 -0600

    Script creación de tablas completado, también se puede eliminar sin problemas

---
+++ CrearTablas.sql
@@ -0,0 +1,141 @@
+CREATE DATABASE bases1_proyecto2
+
+USE bases1_proyecto2
+
+ALTER TABLE Empleado
+DROP CONSTRAINT FK_Empleado_Restaurante
+
+ALTER TABLE Empleado
+DROP CONSTRAINT FK_Empleado_Puesto
+
+ALTER TABLE Item
+DROP CONSTRAINT FK_Item_Orden
+
+DROP TABLE IF EXISTS Restaurante
+CREATE TABLE Restaurante (
+	ID VARCHAR(200) PRIMARY KEY NOT NULL,
+	Direccion VARCHAR(200) NOT NULL,
+	Municipio VARCHAR(200) NOT NULL,
+	Zona INT NOT NULL,
+	Telefono INT NOT NULL,
+	Personal INT NOT NULL,
+	Parqueo BIT NOT NULL
+);
+GO
+
+DROP TABLE IF EXISTS Puesto
+CREATE TABLE Puesto (
+	ID INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+	Nombre VARCHAR(200) NOT NULL,
+	Descripcion VARCHAR(200) NOT NULL,
+	Salario INT NOT NULL
+);
+GO
+
+DROP TABLE IF EXISTS Cliente
+CREATE TABLE Cliente (
+	DPI BIGINT PRIMARY KEY NOT NULL,
+	Nombre VARCHAR(200) NOT NULL,
+	Apellido VARCHAR(200) NOT NULL,
+	FechaNacimiento DATE NOT NULL,
+	Correo VARCHAR(200) NOT NULL,
+	Telefono INT NOT NULL,
+	NIT INT,
+);
+GO
+
+DROP TABLE IF EXISTS Empleado
+CREATE TABLE Empleado (
+	--Este ID debe de ir con 8 dígitos por lo cual necesitamos un trigger que haga eso, es incremental en 1 y empieza directamente en 1
+	ID INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+	Nombre VARCHAR(200) NOT NULL,
+	Apellido VARCHAR(200) NOT NULL,
+	FechaNacimiento DATE NOT NULL,
+	Correo VARCHAR(200) NOT NULL,
+	Telefono INT NOT NULL,
+	Direccion VARCHAR(200) NOT NULL,
+	DPI BIGINT NOT NULL,
+	IDPuesto INT NOT NULL,
+	FechaInicio DATE NOT NULL,
+	IDRestaurante VARCHAR(200) NOT NULL,
+	CONSTRAINT FK_Empleado_Restaurante FOREIGN KEY (IDRestaurante) REFERENCES Restaurante (ID),
+	CONSTRAINT FK_Empleado_Puesto FOREIGN KEY (IDPuesto) REFERENCES Puesto (ID)
+);
+GO
+
+DROP TABLE IF EXISTS Direccion
+CREATE TABLE Direccion (
+	ID INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+	DPICliente BIGINT,
+	Direccion VARCHAR(200) NOT NULL,
+	Municipio VARCHAR(200) NOT NULL,
+	Zona INT
+);
+GO
+
+DROP TABLE IF EXISTS Orden
+CREATE TABLE Orden (
+	ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	IDCliente BIGINT NOT NULL,
+	Canal CHAR(1) NOT NULL,
+	IdDireccion VARCHAR(200) NOT NULL,
+	FechaInicio DATETIME DEFAULT GETDATE() NOT NULL,
+	FechaEntrega DATETIME,
+	Estado VARCHAR(200) NOT NULL,
+	IDRestaurante VARCHAR(200) NOT NULL,
+	IDRepartidor BIGINT
+);
+GO
+
+DROP TABLE IF EXISTS Item
+CREATE TABLE Item (
+	ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	TipoProducto CHAR(1) NOT NULL,
+	Producto INT NOT NULL,
+	Cantidad INT NOT NULL,
+	IDOrden INT NOT NULL,
+	Observacion VARCHAR(200) NOT NULL,
+	CONSTRAINT FK_Item_Orden FOREIGN KEY (IDOrden) REFERENCES Orden (ID)
+);
+GO
+
+DROP TABLE IF EXISTS Factura
+CREATE TABLE Factura (
+	IDFactura INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	NumeroSerie VARCHAR(200) NOT NULL,
+	MontoTotal DECIMAL(10, 2) NOT NULL,
+	Lugar VARCHAR(200) NOT NULL,
+	FechaHoraActual DATETIME NOT NULL,
+	IdOrden INT NOT NULL,
+	NITCliente VARCHAR(25) NOT NULL,
+	FormaPago CHAR(1) NOT NULL
+);
+GO
+
+DROP TABLE IF EXISTS Historial
+CREATE TABLE Historial (
+	ID INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+	Fecha DATETIME DEFAULT GETDATE() NOT NULL,
+	Tipo VARCHAR(200) NOT NULL
+);
+GO
+
+DROP TABLE IF EXISTS Menu
+CREATE TABLE Menu(
+	ID INT PRIMARY KEY NOT NULL,
+	Nombre VARCHAR(200) NOT NULL,
+	Precio DECIMAL(8,2) NOT NULL
+);
+GO
+
+SELECT 
+    f.name AS ForeignKey,
+    OBJECT_NAME(f.parent_object_id) AS TableName,
+    COL_NAME(fc.parent_object_id, fc.parent_column_id) AS ColumnName,
+    OBJECT_NAME (f.referenced_object_id) AS ReferenceTableName
+FROM 
+    sys.foreign_keys AS f
+INNER JOIN 
+    sys.foreign_key_columns AS fc ON f.OBJECT_ID = fc.constraint_object_id
+WHERE 
+    OBJECT_NAME(f.parent_object_id) = 'NombreDeLaTabla'

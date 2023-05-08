e29d53f497ec0b8b29f6d39e7f2431d1c2fed798
Elder Pum <ElderPum@gmail.com>
2023-05-07 23:01:45 -0600

    Procedimientos implementados, archivo prueba funciona todo menos ordenes falta corregir eso

---
+++ Procedimientos.sql
@@ -0,0 +1,511 @@
+USE bases1_proyecto2
+GO
+
+-- Procedimiento para agregar un Restaurante a la DB
+DROP PROCEDURE IF EXISTS AddRestaurante
+GO
+CREATE PROCEDURE AddRestaurant
+    @p_Id VARCHAR(100),
+    @Direccion VARCHAR(200),
+    @Municipio VARCHAR(200),
+    @Zona INT,
+    @Telefono BIGINT,
+    @Personal INT,
+    @Tiene_Parqueo BIT
+AS
+BEGIN
+    -- Primero valido que la zona sea un entero positivo
+    IF dbo.ValidarPositivo(@Zona) = 0 
+		BEGIN
+        SELECT 'ZONA DEBE SER UN NUMERO POSITIVO' AS ERROR;
+        RETURN
+		END
+    -- Validar que el numero de empleados sea un entero positivo 
+    IF dbo.ValidarPositivo(@Personal) = 0 
+		BEGIN
+        SELECT 'NUMERO DE EMPLEADOS DEBE SER UN NUMERO POSITIVO' AS ERROR;
+        RETURN;
+		END
+    -- Valido que el numero en parqueo sea 0 o 1 
+    IF dbo.ValidarBooleano(@Tiene_Parqueo) = 0 
+		BEGIN
+        SELECT 'PARQUEO DEBE SER 0 O 1 UNICAMENTE' AS ERROR
+        RETURN
+		END
+
+    IF EXISTS(SELECT ID FROM dbo.Restaurante WHERE ID = @p_Id) 
+		BEGIN
+        SELECT 'RESTAURANTE YA EXISTE' AS ERROR;
+        RETURN;
+		END;
+
+    INSERT INTO dbo.Restaurante (Id, Direccion, Municipio, Zona, Telefono, Personal, Parqueo) 
+    VALUES (@p_Id, @Direccion, @Municipio, @Zona, @Telefono, @Personal, @Tiene_parqueo);
+END;
+GO
+
+-- Procedimiento para agregar un puesto a la DB
+DROP PROCEDURE IF EXISTS AddPosition
+GO
+CREATE PROCEDURE AddPosition 
+  @p_Nombre VARCHAR(200),
+  @Descripcion VARCHAR(200),
+  @Salario INT
+AS
+BEGIN
+  -- Primero valido que el salario sea un entero positivo
+  IF dbo.ValidarPositivo(@Salario) = 0 
+  BEGIN
+    SELECT 'SALARIO DEBE SER UN NUMERO POSITIVO' AS ERROR;
+    RETURN;
+  END;
+  
+  -- Validar que el id del puesto no existe ya
+  IF EXISTS(SELECT Nombre FROM dbo.Puesto WHERE Nombre = @p_Nombre)
+  BEGIN
+    SELECT 'PUESTO YA INGRESADO' AS ERROR;
+    RETURN;
+  END;
+  
+  INSERT INTO dbo.Puesto(Nombre, Descripcion, Salario) 
+  VALUES (@p_Nombre, @Descripcion, @Salario);
+END;
+GO
+
+-- Procedimiento para añadir nuevos empleados a la DB
+DROP PROCEDURE IF EXISTS AddWorker;
+GO
+CREATE PROCEDURE AddWorker (
+  @Nombres VARCHAR(200),
+  @Apellidos VARCHAR(200),
+  @FechaNacimiento DATE,
+  @Correo VARCHAR(100),
+  @Telefono INT ,
+  @Direccion VARCHAR(100),
+  @p_NumDPI BIGINT,
+  @p_IdPuesto INT,
+  @FechaInicio DATE,
+  @p_IdRestaurante VARCHAR(100)
+)
+AS
+BEGIN
+	-- Primero valido que la zona sea un entero positivo
+	IF dbo.ValidarCorreo(@Correo) = 0 BEGIN
+		SELECT 'EL CORREO NO ES VALIDO' AS ERROR;
+		RETURN;
+	END
+	/* VALIDAR QUE EL ID DEL PUESTO EXISTA  */
+	IF (NOT EXISTS(SELECT Nombre FROM dbo.Puesto WHERE Id = @p_IdPuesto)) BEGIN
+	SELECT 'EL PUESTO INGRESADO NO EXISTE' AS ERROR;
+	RETURN;
+	END
+	/* VALIDAR QUE EL ID DEL RESTAURANTE EXISTA  */
+	IF (NOT EXISTS(SELECT Id FROM dbo.Restaurante WHERE Id = @p_IdRestaurante)) BEGIN
+	SELECT 'EL RESTAURANTE INGRESADO NO EXISTE' AS ERROR;
+	RETURN;
+	END
+	/* VALIDAR QUE EL DPI DEL EMPLEADO NO EXISTA YA */
+	IF (EXISTS(SELECT Nombre FROM dbo.Empleado WHERE DPI = @p_NumDPI)) BEGIN
+	SELECT 'EL EMPLEADO YA ESTA INGRESADO' AS ERROR;
+	RETURN;
+	END
+
+	INSERT INTO dbo.Empleado(Nombre, Apellido, FechaNacimiento, Correo, Telefono, Direccion, DPI, IDPuesto, FechaInicio, IDRestaurante) 
+	VALUES (@Nombres, @Apellidos, @FechaNacimiento, @Correo, @Telefono, @Direccion, @p_NumDPI, @p_IdPuesto, @FechaInicio, @p_IdRestaurante);
+END
+GO
+
+-- Procedimiento para añadir nuevos clientes a la DB
+DROP PROCEDURE IF EXISTS AddClient;
+GO
+CREATE PROCEDURE AddClient (
+  @p_DPI BIGINT,
+  @Nombre VARCHAR(255),
+  @Apellidos VARCHAR(255),
+  @Fecha_de_nacimiento DATE,
+  @Correo VARCHAR(255),
+  @Telefono INT,
+  @NIT INT
+)
+AS
+BEGIN
+	-- Validar que el correo sea válido
+	IF (CHARINDEX('@', @Correo) = 0 OR CHARINDEX('.', @Correo) = 0) BEGIN
+		SELECT 'EL CORREO NO ES VALIDO' AS ERROR;
+		RETURN;
+	END;
+
+	-- Validar que el DPI del cliente no exista ya
+	IF (EXISTS(SELECT Nombre FROM dbo.Cliente WHERE DPI = @p_DPI)) BEGIN
+		SELECT 'EL CLIENTE YA EXISTE' AS ERROR;
+		RETURN;
+	END;
+
+	INSERT INTO dbo.Cliente (DPI, Nombre, Apellido, FechaNacimiento, Correo, Telefono, NIT) 
+	VALUES (@p_DPI, @Nombre, @Apellidos, @Fecha_de_nacimiento, @Correo, @Telefono, @NIT);
+END;
+GO
+
+-- Procedimiento para añadir nuevas direcciones en la DB
+DROP PROCEDURE IF EXISTS AddDirection;
+GO
+CREATE PROCEDURE AddDirection (
+  @p_DPI BIGINT,
+  @Direccion VARCHAR(255),
+  @Municipio VARCHAR(255),
+  @Zona INT
+)
+AS
+BEGIN
+	-- Primero valido que la zona sea un entero positivo
+	IF (@Zona <= 0) BEGIN
+		SELECT 'ZONA DEBE SER UN NUMERO POSITIVO' AS ERROR;
+		RETURN;
+	END;
+	
+	/* VALIDAR QUE EL DPI DEL CLIENTE NO EXISTA YA */
+	IF (NOT EXISTS(SELECT DPI FROM dbo.Cliente WHERE DPI = @p_DPI)) BEGIN
+		SELECT 'EL CLIENTE NO EXISTE' AS ERROR;
+		RETURN;
+	END;
+
+	INSERT INTO dbo.Direccion(DPICliente, Direccion, Municipio, Zona) 
+	VALUES (@p_DPI, @Direccion, @Municipio, @Zona);
+END;
+GO
+
+-- Procedimiento para añadir nuevas ordenes a la DB
+DROP PROCEDURE IF EXISTS AddOrder
+GO
+CREATE PROCEDURE AddOrder (
+  @p_Id BIGINT,
+  @IdCliente BIGINT,
+  @Canal CHAR(1)
+)
+AS
+BEGIN
+	/* DECLARO VARIABLES PARA GUARDAR LA INFO  */
+    DECLARE @municipioCliente VARCHAR(255);
+    DECLARE @zonaCliente INT;
+    DECLARE @idRestaurante VARCHAR(100);
+
+    /* GUARDO LA RESPUESTA EN MIS VARIABLES municipioCliente, zonaCliente  */
+    SELECT @municipioCliente = Municipio, @zonaCliente = Zona FROM dbo.Direccion WHERE Id = @IdCliente;
+
+    /* GUARDO LA RESPUESTA EN MI VARIABLE idRestaurante  */
+    SELECT @idRestaurante = Id FROM dbo.Restaurante WHERE Municipio = @municipioCliente AND Zona = @zonaCliente;
+
+    /* VALIDAR QUE EL DPI DEL CLIENTE EXISTA */
+    IF (NOT EXISTS(SELECT Nombre FROM dbo.Cliente WHERE DPI = @p_Id)) 
+    BEGIN
+        SELECT 'EL CLIENTE NO EXISTE' AS ERROR;
+        RETURN;
+    END
+
+    /* VALIDAR QUE EL CANAL SEA PERMITIDO */
+    IF dbo.ValidarCanal(@Canal) = 0 
+    BEGIN
+        SELECT 'CANAL NO VALIDO' AS ERROR;
+        RETURN;
+    END
+
+    IF (NOT EXISTS(SELECT Municipio FROM dbo.Direccion WHERE DPICliente = @p_Id AND Id = @IdCliente)) 
+    BEGIN
+        SELECT 'LA DIRECCION ES DE OTRO CLIENTE' AS ERROR;
+        RETURN;
+    END
+
+    IF @idRestaurante IS NULL 
+    BEGIN
+        INSERT INTO dbo.Orden (IDCliente, Canal, Estado) VALUES (@p_Id, @Canal, 'SIN COBERTURA');
+    END
+    ELSE
+    BEGIN
+        INSERT INTO dbo.Orden (IDCliente, IdDireccion, IDRestaurante, Canal, Estado) VALUES (@p_Id, @IdCliente, @idRestaurante, @Canal, 'INICIADA');
+    END
+END;
+GO
+
+-- Procedimiento para agregar los items a la DB
+DROP PROCEDURE IF EXISTS AddItem
+GO
+CREATE PROCEDURE AddItem 
+  @idOrden INT,
+  @tipo_prod CHAR(1),
+  @producto INT,
+  @cantidad INT,
+  @Observacion VARCHAR(200)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	DECLARE @estadoOrden VARCHAR(50);
+
+	/* VALIDAR QUE LA ORDEN EXISTA  */
+	IF (NOT EXISTS(SELECT Id FROM dbo.Orden WHERE Id = @idOrden )) BEGIN
+		SELECT 'LA ORDEN NO EXISTE' AS ERROR;
+		RETURN
+	END
+
+	/* VALIDAR QUE LA ORDEN EXISTA  */
+	IF (NOT EXISTS(SELECT Id FROM dbo.Orden WHERE Id = @idOrden AND (Estado = 'INICIADA' OR Estado = 'AGREGANDO') )) BEGIN
+		SELECT 'EL ESTADO NO PERMITE AGREGAR ITEMS' AS ERROR;
+		RETURN
+	END
+
+	/* OBTENGO EL ESTADO DE LA ORDEN */
+    SELECT @estadoOrden = Estado FROM dbo.Orden WHERE Id = @idOrden;
+   
+    /* VALIDO QUE LA ORDEN NO ESTÉ EN ESTADO FINALIZADA O EN CAMINO */
+    IF @estadoOrden = 'FINALIZADA' OR @estadoOrden = 'EN CAMINO' BEGIN
+        SELECT 'NO SE PUEDE AGREGAR PRODUCTO A UNA ORDEN FINALIZADA O EN CAMINO' AS ERROR;
+        RETURN
+    END
+   
+    /* ACTUALIZO EL ESTADO DE LA ORDEN A "AGREGANDO" SI SE ENCONTRABA EN "INICIADA" */
+    IF @estadoOrden = 'INICIADA' BEGIN
+        UPDATE dbo.Orden SET Estado = 'AGREGANDO' WHERE Id = @idOrden;
+    END
+	
+	/* VALIDAR QUE EL TIPO DE PRODUCTO SEA PERMITDIO */
+	IF dbo.ValidarTipoProd(@tipo_prod) = 0 BEGIN 
+		SELECT 'TIPO DE PRODUCTO NO PERMITIDO' AS ERROR;
+		RETURN
+	END
+
+	/* VALIDAR QUE CANTIDAD SE ENTERO POSITIVO */
+	IF dbo.ValidarPositivo(@cantidad) = 0 BEGIN 
+			SELECT 'CANTIDAD DEBE SER UN NUMERO POSITIVO' AS ERROR;
+			RETURN
+	END
+
+	/* VALIDAR QUE SEA UN PRODUCTO QUE EXISTA */
+	IF (NOT EXISTS(SELECT Nombre FROM dbo.Menu WHERE Id = CONCAT(@tipo_prod, @producto))) BEGIN
+		SELECT 'EL PRODUCTO NO EXISTE' AS ERROR; 
+		RETURN
+	END
+		
+	INSERT INTO dbo.Item (TipoProducto, Producto, Cantidad, IDOrden, Observacion) 
+    VALUES (@tipo_prod, @producto, @cantidad, @idOrden, @Observacion);
+END;
+GO
+
+-- Procedimiento para confirmar ordenes de la DB
+DROP PROCEDURE IF EXISTS ConfirmOrder
+GO
+CREATE PROCEDURE ConfirmOrder (
+  @idOrden INT,
+  @FormaPago CHAR(1),
+  @p_IdRepartidor INT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	/*DECLARAMOS TODO PARA LA FACTURA*/
+	DECLARE @p_total DECIMAL(10,2);
+	DECLARE @p_numserie VARCHAR(50);
+	DECLARE @p_lugar VARCHAR(200);
+	DECLARE @p_fecha_hora DATETIME;
+	DECLARE @p_nit INT;
+	DECLARE @p_FormaPago CHAR(1);
+	DECLARE @p_estado_orden VARCHAR(50);
+	
+	/* VALIDAR QUE LA ORDEN EXISTA  */
+	IF (NOT EXISTS(SELECT Id FROM dbo.Orden WHERE Id = @idOrden )) BEGIN
+	    SELECT 'LA ORDEN NO EXISTE' AS ERROR;
+	    RETURN;
+	END;
+	
+	/* VALIDAR QUE LA FORMA DE PAGO ES CORRECTA */
+	IF dbo.ValidarFormaPago(@FormaPago) = 0 BEGIN 
+		SELECT 'FORMA DE PAGO NO ADMITIDO' AS ERROR;
+		RETURN;
+	END;
+	
+	/* VALIDAR QUE EL TRABAJADOR EXISTA  */
+	IF (NOT EXISTS(SELECT Id FROM dbo.Empleado WHERE Id = @p_IdRepartidor )) BEGIN
+	    SELECT 'NO EXISTE EL TRABAJADOR' AS ERROR;
+	    RETURN;
+	END;
+	
+	/* OBTENGO EL ESTADO DE LA ORDEN */
+    SELECT @p_estado_orden = Estado FROM dbo.Orden WHERE Id = @idOrden;
+    
+    /* VALIDO QUE LA ORDEN NO ESTÉ EN ESTADO ENTREGADA O EN CAMINO */
+    IF (@p_estado_orden != 'AGREGANDO') BEGIN
+        SELECT 'NO SE PUEDE CONFIRMAR LA ORDEN AHORA' AS ERROR;
+        RETURN;
+    END;
+    
+    UPDATE dbo.Orden SET Estado = 'EN CAMINO', IdRepartidor = @p_IdRepartidor WHERE Id = @idOrden;
+
+	/* OBTENGO LOS DATOS DE LA ORDEN Y DEL CLIENTE */	
+ 	SET @p_lugar = (SELECT Municipio FROM dbo.Direccion WHERE DPICliente = (SELECT IdCliente FROM dbo.Orden WHERE Id = @idOrden));
+ 	SET @p_nit = (SELECT NIT FROM dbo.Cliente WHERE DPI = (SELECT IdCliente FROM dbo.Orden WHERE Id = @idOrden));
+END;
+GO
+
+DROP PROCEDURE IF EXISTS FinishOrder
+GO
+CREATE PROCEDURE FinishOrder
+  @idOrden INT
+AS
+BEGIN
+  DECLARE @p_estado_orden VARCHAR(50);
+	
+  -- VALIDAR QUE LA ORDEN EXISTA
+  IF NOT EXISTS(SELECT ID FROM dbo.Orden WHERE ID = @idOrden) 
+  BEGIN
+    SELECT 'LA ORDEN NO EXISTE' AS ERROR;
+    RETURN;
+  END;
+
+  -- OBTENGO EL ESTADO DE LA ORDEN
+  SELECT @p_estado_orden = Estado FROM dbo.Orden WHERE Id = @idOrden;
+   
+  -- VALIDO QUE LA ORDEN NO ESTÉ EN ESTADO ENTREGADA O EN CAMINO
+  IF @p_estado_orden <> 'EN CAMINO' 
+  BEGIN
+    SELECT 'NO SE PUEDE FINALIZAR LA ORDEN PORQUE NO ESTÁ EN CAMINO' AS ERROR;
+    RETURN;
+  END;
+
+  UPDATE dbo.Orden SET Estado = 'ENTREGADA', FechaEntrega = GETDATE() WHERE Id = @idOrden;
+END;
+GO
+
+DROP PROCEDURE IF EXISTS GetRestaurants
+GO
+CREATE PROCEDURE GetRestaurants
+AS
+BEGIN
+    SELECT Id, Direccion, Municipio, Zona, Telefono, Personal,
+        CASE Parqueo WHEN 1 THEN 'Sí' ELSE 'No' END AS Tiene_Parqueo
+    FROM dbo.Restaurante;
+END;
+GO
+
+-- Procedimiento para obtener los datos de un empleado
+DROP PROCEDURE IF EXISTS GetEmployee
+GO
+CREATE PROCEDURE GetEmployee
+    @idEmpleado INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	
+	/* VALIDAR QUE EL EMPLEADO NO EXISTA  */
+	IF NOT EXISTS(SELECT Id FROM dbo.Empleado WHERE Id = @idEmpleado )
+	BEGIN
+	    SELECT 'EL CLIENTE NO EXISTE' AS ERROR;
+	    RETURN;
+	END;
+	
+	SELECT e.Id, CONCAT(e.Nombre ,' ', e.Apellido) AS NombreCompleto, e.FechaNacimiento, e.Correo, e.Telefono, e.Direccion, e.DPI, e.FechaInicio, p.Nombre, p.Salario
+	FROM dbo.Empleado e
+	INNER JOIN dbo.Puesto p ON e.IdPuesto = p.Id
+	WHERE e.Id = @idEmpleado;
+	
+END;
+GO
+
+-- Procedimiento para obtener los items de las ordenes
+DROP PROCEDURE IF EXISTS GetItemOrder
+GO
+CREATE PROCEDURE GetItemOfOrder (
+    @idOrder INT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    /* VALIDAR QUE LA ORDEN EXISTA */
+    IF NOT EXISTS (SELECT Id FROM dbo.Orden WHERE Id = @idOrder)
+    BEGIN
+        SELECT 'LA ORDEN NO EXISTE' AS ERROR;
+        RETURN;
+    END;
+
+    /* VALIDAR QUE LA ORDEN SEA DIFERENTE A SIN COBERTURA */
+    IF EXISTS (SELECT Id FROM dbo.Orden WHERE Id = @idOrder AND Estado = 'SIN COBERTURA')
+    BEGIN
+        SELECT 'ESTA ORDEN NO TIENE COBERTURA' AS ERROR;
+        RETURN;
+    END;
+
+    /* LE PONGO NOMBRE AL TIPO DE PRODUCTO */
+    SELECT p.Nombre AS Producto,
+           CASE i.TipoProducto
+                WHEN 'C' THEN 'Combo'
+                WHEN 'E' THEN 'Extra'
+                WHEN 'B' THEN 'Bebida'
+                WHEN 'P' THEN 'Postre'
+           END AS TipoProducto,
+           p.Precio AS Precio,
+           i.cantidad AS Cantidad,
+           i.observacion AS Observacion
+    FROM dbo.Item i
+    INNER JOIN dbo.Menu p ON CONCAT(i.TipoProducto, producto) = p.Id
+    WHERE i.idOrden = @idOrder;
+
+END;
+GO
+
+-- Procedimiento para obtener las direcciones de un cliente almacenado en la DB
+DROP PROCEDURE IF EXISTS GetAddress
+GO
+CREATE PROCEDURE GetAddress
+    @dpi_client BIGINT
+AS
+BEGIN
+    -- VALIDAR QUE EL CLIENTE EXISTA
+    IF NOT EXISTS (SELECT DPI FROM dbo.Cliente WHERE DPI = @dpi_client)
+    BEGIN
+        SELECT 'EL CLIENTE NO EXISTE' AS ERROR;
+        RETURN;
+    END;
+
+    -- OBTENER LOS DATOS DE LA DIRECCIÓN
+    SELECT Direccion, Municipio, Zona
+    FROM dbo.Direccion
+    WHERE DPICliente = @dpi_client;
+END;
+GO
+
+DROP PROCEDURE IF EXISTS GetOrderByState
+GO
+CREATE PROCEDURE GetOrdersByState
+    @EstadoOrden INT
+AS
+BEGIN
+    SELECT o.Id AS IdOrden, o.Estado, o.FechaInicio AS Fecha, o.IdCliente AS DPICliente, o.IdDireccion,
+           CASE o.canal
+               WHEN 'L' THEN 'Llamada'
+               WHEN 'A' THEN 'Aplicacion'
+           END AS Canal, r.Direccion AS Restaurante
+    FROM dbo.Orden o 
+    LEFT OUTER JOIN dbo.Restaurante r ON r.Id = o.IdDireccion
+    WHERE o.Estado = CASE @EstadoOrden
+                         WHEN 1 THEN 'INICIADA'
+                         WHEN 2 THEN 'AGREGANDO'
+                         WHEN 3 THEN 'EN CAMINO'
+                         WHEN 4 THEN 'ENTREGADA'
+                         WHEN -1 THEN 'SIN COBERTURA'
+                     END;
+END;
+GO
+
+DROP PROCEDURE IF EXISTS GetTimes
+GO
+
+CREATE PROCEDURE GetTimes
+    @Minutos INT
+AS
+BEGIN
+    SELECT o.Id as IdOrden, o.IdDireccion as DireccionEntrega, o.FechaInicio, DATEDIFF(MINUTE, o.FechaInicio, o.FechaEntrega) AS TiempoEspera, CONCAT(e.Nombre,' ',e.Apellido) AS Repartidor
+    FROM dbo.Orden o
+    LEFT JOIN dbo.Empleado e ON o.IdRepartidor = e.Id
+    WHERE o.Estado = 'ENTREGADA' AND DATEDIFF(MINUTE, o.FechaInicio, o.FechaEntrega) >= @Minutos;
+END
+GO
---
+++ Prueba.sql
@@ -0,0 +1,125 @@
+use bases1_proyecto2
+
+-- ########################### INGRESAR Restaurante ###########################
+EXEC AddRestaurant 'R-01', 'Zona 10, Guatemala City','Guatemala',-10,12121212,10,1; -- Zona invalida
+EXEC AddRestaurant 'R-01', 'Zona 10, Guatemala City','Guatemala',10,12121212,-10,1; -- personal invalido
+EXEC AddRestaurant 'R-03', 'Zona 20, Villa Nueva','Guatemala',10,12121223,10,1; -- ok
+EXEC AddRestaurant 'R-02', 'Zona 19, San Miguel Petapa','Guatemala',10,12121213,10,1; -- ok
+EXEC AddRestaurant 'R-01', 'Zona 10, Guatemala City','Guatemala',10,12121212,10,1; -- error duplicado
+
+
+
+-- ########################### INGRESAR Puesto De Trabajo ###########################
+EXEC AddPosition 'Gerente','Monitoreo del restaurante',-5000.00; -- Salario Invalido
+EXEC AddPosition 'Gerente','Monitoreo del restaurante',5000.00;  -- ok
+EXEC AddPosition 'Gerente','Monitoreo del restaurante',5000.00; -- Error repetido
+EXEC AddPosition 'Repartidor','Repartir comida',3500.00; -- ok
+EXEC AddPosition 'Conserje','Limpieza de restaurante',3000.00; -- ok
+
+
+-- ########################### INGRESAR Empleados ###########################
+EXEC AddWorker 'Harry','Maguire','1990-01-01','hola.com',12347890,'Zona 15',4567098710234,1,'2022-01-01',"R-01"; -- Correo invalido
+EXEC AddWorker 'Harry','Maguire','1990-01-01','hola@gmail.com',12347890,'Zona 15',4567098710234,4,'2022-01-01',"R-01"; -- Error, puesto de trabajo inexistente
+EXEC AddWorker 'Harry','Maguire','1990-01-01','hola@gmail.com',12347890,'Zona 15',4567098710234,1,'2022-01-01',"R-02"; -- Error, restaurante inexistente
+EXEC AddWorker 'Jennie','Maguire','1990-01-01','hola2@gmail.com',12347890,'Zona 21',3000346440101,7,'2022-01-01',"R-01"; -- Ok
+EXEC AddWorker 'James','Hollywood','1990-01-01','aaa@hotmail.es',12347899,'Zona 15',4567098710234,1,'2022-01-01',"R-01"; -- Error, dpi duplicado
+EXEC AddWorker 'James','Hollywood','1990-01-01','aaa@hotmail.es',12347899,'Zona 15',4567098710235,3,'2022-01-01',"R-01"; -- Ok
+
+-- ########################### INGRESAR Clientes ###########################
+EXEC AddClient 1234678909123,'Alex','Hunter','1990-01-01','bbbbb@',89709867,NULL; -- Correo Invalido
+EXEC AddClient 1234678909135,'Alex','Hunter2','1990-01-01','bbbbb@gmail.com',89709867,NULL; -- ok
+EXEC AddClient 1234678909123,'Calvin','Murder','1990-01-01','ccccc@gmail.com',89709867,NULL; -- Error, Dpi duplicado
+EXEC AddClient 1234678909097,'Calvin','Murder','1990-01-01','ccccc@gmail.com',89709867,89765678; -- ok
+
+
+-- ########################### INGRESAR Direcciones ###########################
+
+EXEC AddDirection 4444444444444,'Zona 24, Guatemala','Guatemala',10; -- Error, dpi inexistente
+EXEC AddDirection 1234678909123,'Zona 24, Guatemala','Guatemala',10; -- ok
+EXEC AddDirection 1234678909123,'Zona 6, Mixco','Mixco',-1; -- Error, zona inv�lida
+EXEC AddDirection 1234678909097,'Zona 6, Mixco','Mixco',6; -- ok
+EXEC AddDirection 1234678909097,'Zona 10, Guatemala','Guatemala',10; -- ok
+
+
+-- ########################### Crear Ordenes ###########################
+EXEC AddOrder 4444444444444,1,'L'; -- Error, dpi inexistente
+EXEC AddOrder 1234678909123,2,'L'; -- Error, dpi si existe, pero esa direccion es de otro cliente
+EXEC AddOrder 1234678909123,1,'L'; -- ok
+EXEC AddOrder 1234678909097,2,'C'; -- Error canal equivocado
+EXEC AddOrder 1234678909097,2,'A'; -- Error, no hay restaurante con cobertura
+EXEC AddOrder 1234678909097,3,'A'; -- ok
+
+
+-- ########################### Agregar Items a ordenes ###########################
+EXEC AddItem 10,'C',1,10,''; -- Error, orden inexistente
+EXEC AddItem 1,'C',1,-10,''; -- Error, cantidad invalida
+EXEC AddItem 1,'O',1,10,''; -- Error, tipo de producto invalido
+EXEC AddItem 1,'C',10,10,''; -- Error, producto inexistente
+EXEC AddItem 6,'C',1,10,''; -- ok
+EXEC AddItem 6,'E',1,10,''; -- ok
+EXEC AddItem 1,'B',1,10,''; -- ok
+EXEC AddItem 1,'P',1,10,''; -- ok
+EXEC AddItem 1,'P',19,10,''; -- Error, producto inexistente
+EXEC AddItem 2,'P',19,10,''; -- Error, no se pueden agregar items a una orden sin cobertura
+EXEC AddItem 2,'C',2,5,''; -- ok
+EXEC AddItem 2,'E',2,5,''; -- ok
+EXEC AddItem 2,'B',2,5,''; -- ok
+EXEC AddItem 2,'P',2,5,''; -- ok
+
+
+-- ########################### Confirmar ordenes ###########################
+EXEC ConfirmOrder 10,'E',3; -- Error,Orden inexistente
+EXEC ConfirmOrder 1,'E',13; -- Error, El trabajador no existe
+EXEC ConfirmOrder 1,'S',2; -- Error, M�todo de pago invalido
+EXEC ConfirmOrder 6,'E',2; -- ok
+EXEC ConfirmOrder 3,'T',2; -- ok
+
+
+-- ########################### Finalizar �rdenes ###########################
+EXEC FinishOrder 1; -- OK
+EXEC FinishOrder 6; -- Error, orden inexistente
+EXEC FinishOrder 2; -- OK
+
+
+-- ########################### REPORTE # 01 ###########################
+EXEC GetRestaurants;
+
+-- ########################### REPORTE # 02 ###########################
+EXEC GetEmployee 5; -- Error Empleado inexistente
+EXEC GetEmployee 1; -- ok
+EXEC GetEmployee 2; -- ok
+EXEC GetEmployee 4; -- Error Empleado inexistente
+
+-- ########################### REPORTE # 03 ###########################
+EXEC GetItemOfOrder 1; -- ok
+EXEC GetItemOfOrder 4; -- Error, esta orden se quedo en estado de "SIN COBERTURA"
+EXEC GetItemOfOrder 3; -- ok
+EXEC GetItemOfOrder 6; -- Error, esta orden no existe
+
+-- ########################### REPORTE # 04 ###########################
+EXEC ConsultarHistorialOrdenes 1234678909123; -- ok
+EXEC ConsultarHistorialOrdenes 44444444444444; -- Error, dpi inexistente              ME FALTA
+EXEC ConsultarHistorialOrdenes 1234678909097; -- ok
+
+-- ########################### REPORTE # 05 ###########################
+EXEC GetAddress 1234678909123; -- ok
+EXEC GetAddress 44444444444444; -- Error, dpi inexistente
+EXEC GetAddress 1234678909097; -- ok
+
+-- ########################### REPORTE # 06 ###########################
+EXEC GetOrdersByState -1; -- Deber�a mostrar una orden sin cobertura
+EXEC GetOrdersByState 1; -- No deber�a mostrar nada
+EXEC GetOrdersByState 2; -- No deber�a mostrar nada
+EXEC GetOrdersByState 3; -- No deber�a mostrar nada
+EXEC GetOrdersByState 4; -- Deber�a mostrar 2 �rdenes entregadas.
+
+-- ########################### REPORTE # 07 ###########################
+-- Editar las fechas seg�n sea el caso de validaci�n
+EXEC ConsultarFacturas 19,04,2023;
+EXEC ConsultarFacturas 29,04,2023;
+
+-- ########################### REPORTE # 08 ###########################
+-- Editar los minutos de parametro segun sea el caso de validaci�n
+EXEC GetTimes 3;
+EXEC GetTimes 10;
+EXEC GetTimes 1;
